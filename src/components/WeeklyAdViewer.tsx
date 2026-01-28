db399b94870687020408fda058f389b234af758b
StratiDaGoat <strati.koulouris@gmail.com>
2026-01-28 13:25:11 -0600

    Weekly Ad Week 5

--- src/components/WeeklyAdViewer.tsx
+++ src/components/WeeklyAdViewer.tsx
@@ -46,8 +46,8 @@ export const WeeklyAdViewer: React.FC<WeeklyAdViewerProps> = ({
   }, []);
 
   const pdfFiles = [
-    { pdf: '/weekly-ad-week-4.pdf', image: '/weekly-ad-week-4-1.webp' },
-    { pdf: '/weekly-ad-week-4.pdf', image: '/weekly-ad-week-4-2.webp' },
+    { pdf: '/weekly-ad-week-5.pdf', image: '/weekly-ad-week-5-1.webp' },
+    { pdf: '/weekly-ad-week-5.pdf', image: '/weekly-ad-week-5-2.webp' },
   ];
 
   const renderPDF = (pdfPath: string, imagePath: string, index: number) => {
